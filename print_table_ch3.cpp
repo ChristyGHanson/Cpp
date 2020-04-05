a696262d2e509ad82b24f8f6b512cd6099ee39fe
Christy G. Hanson <34938344+ChristyGHanson@users.noreply.github.com>
2020-04-04 18:12:26 -0700

    Practice: Strings

---
+++ print_table_ch3.cpp
@@ -0,0 +1,12 @@
+// g++ -Wall -Wextra print_tale_ch3.cpp -o print_table_ch3.exe
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "   Monster |   Name            \n";
+    cout << "===========|===================\n";
+    cout << " Werewolf  | Remus Lupin       \n";
+    cout << " Vampire   | Dr. Acula         \n";
+    cout << " Old God   | Mike \"Cthulhu\" Jones\n";
+}
